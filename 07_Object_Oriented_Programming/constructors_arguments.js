5e6b7f404ae33d32e7ef5dccb201710c8bb5412a
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-13 19:43:24 -0300

    Object Oriented Programming

---
+++ 07_Object_Oriented_Programming/constructors_arguments.js
@@ -0,0 +1,9 @@
+// Estender construtores para receber argumentos
+
+function Dog(name, color) {
+  this.name = name; 
+  this.color = color;
+  this.numLegs = 4; 
+}; 
+
+let terrier = new Dog("Alaska", "White");
