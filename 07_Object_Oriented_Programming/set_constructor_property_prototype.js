4a284deaaa24baf361ce9f092d83dac359408e73
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-13 20:45:51 -0300

    Object Oriented Programming

---
+++ 07_Object_Oriented_Programming/set_constructor_property_prototype.js
@@ -0,0 +1,16 @@
+// Lembrar de definir a propriedade construtora quando alterar o protótipo
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  constructor: Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
