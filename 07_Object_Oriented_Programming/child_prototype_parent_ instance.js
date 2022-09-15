acf1b9f0ae1dab9c482b09f12fbe2e9430006cd9
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-14 21:22:38 -0300

    Object Oriented Programming

---
+++ 07_Object_Oriented_Programming/child_prototype_parent_ instance.js
@@ -0,0 +1,18 @@
+// Definir o protótipo da classe filha para que seja uma instância do pai
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
+
+beagle.eat();
