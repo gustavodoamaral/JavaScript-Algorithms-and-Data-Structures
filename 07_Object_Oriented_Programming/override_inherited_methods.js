ddee18900306d5cc2bc81b230f141edc209b8e91
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-15 20:58:58 -0300

    Object Oriented Programming

--- 07_Object_Oriented_Programming/override_inherited_methods.js
+++ 07_Object_Oriented_Programming/override_inherited_methods.js
@@ -8,13 +8,9 @@ function Penguin() { }
 Penguin.prototype = Object.create(Bird.prototype);
 Penguin.prototype.constructor = Penguin;
 
-// Altere apenas o código abaixo desta linha
-
 Penguin.prototype.fly = function(){
   return "Alas, this is a flightless bird."
 }
-
-// Altere apenas o código acima desta linha
 
 let penguin = new Penguin();
 console.log(penguin.fly());
