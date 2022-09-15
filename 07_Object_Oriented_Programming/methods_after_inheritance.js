a5e840278583704c4d5c40e1f98e17fe86ab8a17
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-15 20:50:35 -0300

    Object Oriented Programming

---
+++ 07_Object_Oriented_Programming/methods_after_inheritance.js
@@ -0,0 +1,16 @@
+// Adicionar métodos após a herança
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+Dog.prototype= Object.create(Animal.prototype)
+
+Dog.prototype.constructor = Dog; 
+
+Dog.prototype.bark = function() {
+  console.log("Woof!")
+}
+
+let beagle = new Dog();
