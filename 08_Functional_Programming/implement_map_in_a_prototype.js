5dd05f70bc69a346bb60ae06c32b0f4db328eb3c
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-21 20:01:27 -0300

    Functional Programming

---
+++ 08_Functional_Programming/implement_map_in_a_prototype.js
@@ -0,0 +1,16 @@
+// Implementar map em um protótipo
+
+// A variável global
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]))
+  }
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
