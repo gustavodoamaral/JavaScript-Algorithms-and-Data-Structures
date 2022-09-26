62b65971f52473f162be2e53bafe21e4dc28dfb5
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-09-26 20:12:28 -0300

    Functional Programming

---
+++ 08_Functional_Programming/sort_an_array_alphabetically.js
@@ -0,0 +1,9 @@
+// Ordenar um array alfabeticamente usando o método sort
+
+function alphabeticalOrder(arr) {
+  return arr.sort(function(a, b) {
+    return a === b ? 0 : a < b ? -1 : 1;
+  });
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
