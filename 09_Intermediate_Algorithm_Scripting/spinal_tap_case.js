1427a08968f65a52b675ce776aa1429b00835fd5
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2022-10-03 20:34:48 -0300

    Intermediate Algorithm Scripting

--- 09_Intermediate_Algorithm_Scripting/spinal_tap_case.js
+++ 09_Intermediate_Algorithm_Scripting/spinal_tap_case.js
@@ -1,10 +1,5 @@
 function spinalCase(str) {
-  //let str = str.split(/(?=[A-Z])/)
-  //str = str.toString()
-  //str = str.replace(/,/g, ' ')
-  //str = str.replace(/[- ]+/g, "-")
-  //str = str.replace(/[_ -]+/g, "-")
-  
+
   var regex = /\s+|_+/g;
   str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
   return str.replace(regex, "-").toLowerCase();
@@ -12,3 +7,9 @@ function spinalCase(str) {
 }
 
 spinalCase('The_Andy_Griffith_Show');
+
+  //let str = str.split(/(?=[A-Z])/)
+  //str = str.toString()
+  //str = str.replace(/,/g, ' ')
+  //str = str.replace(/[- ]+/g, "-")
+  //str = str.replace(/[_ -]+/g, "-")
